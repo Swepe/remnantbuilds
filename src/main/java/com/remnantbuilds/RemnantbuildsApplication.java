4e368fbb99de2788b0f52c50e336b2711ebd0b39
Swepe <99968893+Swepe@users.noreply.github.com>
2023-06-16 18:01:54 +0200

    initial

---
+++ src/main/java/com/remnantbuilds/RemnantbuildsApplication.java
@@ -0,0 +1,13 @@
+package com.remnantbuilds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemnantbuildsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RemnantbuildsApplication.class, args);
+    }
+
+}
