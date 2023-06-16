354a9ef1439d19bf7673670b1f46c5e6ae20e365
Swepe <sitkeryivan@gmail.com>
2023-06-16 14:15:53 +0200

    initial

---
+++ src/test/java/com/remnantbuilds/RemnantBuildsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.remnantbuilds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RemnantBuildsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
